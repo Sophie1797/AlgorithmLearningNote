45ad98c6027a6bbb7f3cc2bdeb0fa80f54e1eee1
Sophie1797 <35486279+Sophie1797@users.noreply.github.com>
2019-06-12 20:55:31 +0800

    add more

--- src/AlgorithmNote/AlgorithmNote/Graph/DenseGraph.cs
+++ src/AlgorithmNote/AlgorithmNote/Graph/DenseGraph.cs
@@ -11,7 +11,7 @@ namespace AlgorithmNote
     /// </summary>
     public class DenseGraph
     {
-        private int n, m;
+        private int n, m;//n:顶点的个数，m:边的个数
         private bool directed;//表示这个图是有向图还是无向图
         private List<List<bool>> graph;
 
@@ -51,11 +51,40 @@ namespace AlgorithmNote
         /// <summary>
         /// 给图中添加一条边
         /// </summary>
-        /// <param name="v"></param>
-        /// <param name="w"></param>
         public void AddEdge(int v, int w)
         {
+            if (v < 0 || v >= n || w < 0 || w >= n)
+            {
+                throw new ArgumentException("Vertex value invalid!");
+            }
+
+            //如果v和w已经有一条边了，就直接return
+            if (HasEdge(v, w))
+            {
+                return;
+            }
+
             graph[v][w] = true;
+            //如果是无向图，则反向边也需要设置
+            if (!directed)
+            {
+                graph[w][v] = true;
+            }
+
+            m++;
+        }
+
+        /// <summary>
+        /// 判断是否有平行边
+        /// </summary>
+        public bool HasEdge(int v, int w)
+        {
+            if (v < 0 || v >= n || w < 0 || w >= n)
+            {
+                throw new ArgumentException("Vertex value invalid!");
+            }
+
+            return graph[v][w];
         }
     }
 }
--- src/AlgorithmNote/AlgorithmNote/Graph/SparseGraph.cs
+++ src/AlgorithmNote/AlgorithmNote/Graph/SparseGraph.cs
@@ -6,8 +6,87 @@ using System.Threading.Tasks;
 
 namespace AlgorithmNote
 {
+    /// <summary>
+    /// 用邻接表实现稀疏图
+    /// </summary>
     public class SparseGraph
     {
+        private int n, m;//n:顶点的个数，m:边的个数
+        private bool directed;//表示这个图是有向图还是无向图
+        private List<List<int>> graph;
 
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="n">顶点个数</param>
+        /// <param name="directed">指示这个图是有向图还是无向图</param>
+        public SparseGraph(int n, bool directed)
+        {
+            this.n = n;
+            this.directed = directed;
+            for (var i = 0; i < n; i++)
+            {
+                graph.Add(new List<int>());
+            }
+        }
+
+        /// <summary>
+        /// 得到图中顶点的个数
+        /// </summary>
+        /// <returns></returns>
+        public int V()
+        {
+            return n;
+        }
+
+        /// <summary>
+        /// 得到图中边的条数
+        /// </summary>
+        /// <returns></returns>
+        public int E()
+        {
+            return m;
+        }
+
+        /// <summary>
+        /// 给图中添加一条边
+        /// </summary>
+        public void AddEdge(int v, int w)
+        {
+            if (v < 0 || v >= n || w < 0 || w >= n)
+            {
+                throw new ArgumentException("Vertex value invalid!");
+            }
+
+            graph[v].Add(w);
+            //如果是自环边，而且是无向图，不需要再添加一次反向的边
+            if (v != w && !directed)
+            {
+                graph[w].Add(v);
+            }
+
+            m++;
+        }
+
+        /// <summary>
+        /// 判断是否有平行边，O(n)
+        /// </summary>
+        public bool HasEdge(int v, int w)
+        {
+            if (v < 0 || v >= n || w < 0 || w >= n)
+            {
+                throw new ArgumentException("Vertex value invalid!");
+            }
+
+            for (var i = 0; i < graph[v].Count; i++)
+            {
+                if (graph[v][i] == w)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
