f76295a8388c11aa478bad7d77037faaec737199
Sophie1797 <35486279+Sophie1797@users.noreply.github.com>
2019-06-19 15:24:10 +0800

    Update IndexHeapTest.cs

--- src/AlgorithmNote/UnitTest/Heap/IndexHeapTest.cs
+++ src/AlgorithmNote/UnitTest/Heap/IndexHeapTest.cs
@@ -33,5 +33,32 @@ namespace UnitTest
 
             Assert.AreEqual(true, success);
         }
+        
+        [TestMethod]
+        public void TestIndexMinHeap_List()
+        {
+            int n = 1000000;
+            Random random = new Random(111);
+            var heap = new IndexMinHeap_List<int>(n);
+            for (int i = 0; i < n; i++)
+                heap.Add(random.Next(1000000));
+
+            var arr = new int[n];
+            for (var i = 0; i < n; i++)
+            {
+                arr[i] = heap.ExtractMin();
+            }
+
+            var success = true;
+            for (var i = 1; i < n; i++)
+            {
+                if (arr[i - 1] > arr[i])
+                {
+                    success = false;
+                }
+            }
+
+            Assert.AreEqual(true, success);
+        }
     }
 }
